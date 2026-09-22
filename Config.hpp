01de33b36dc6218d50fcf301b481dccf92c64298
cchristosgalanis <galanischris2@yahoo.com>
2026-09-22 13:27:18 +0300

    adding periodic functions approach

--- Config.hpp
+++ Config.hpp
@@ -1,21 +1,24 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
+#include<cmath>
 
 namespace Config {
     // parameters of the Bernoulli function
-    constexpr double x_start = 1.0; // ceiling
-    constexpr double x_end = 5.0; // bottom
+    constexpr double x_start = 1.0; // start
+    constexpr double x_end = 10.0; // end
     constexpr int N_points = 100; // points of grid
     constexpr double y_0 = 2.0; // initial solution for x = x_start
     constexpr double alpha = 2.0; // bernoulli power
+    constexpr double omega = 1.0; // parameter for different frequencies
 
     // functions P(x) & Q(x)
     static double P(double x){
-        return 1.0/x;
+
+        return std::cos(omega * x);
     }
 
     static double Q(double x){
-        return 1.0;
+        return std::sin(omega * x);
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -24,51 +24,54 @@ int main(){
     solver.euler_solver(N_points, psi.data(), Config::y_0, Config::alpha, Config::x_start, Config::x_end, Config::P, Config::Q, grid.data());
 
     // call function for analytical solution
-    solver.analytical_method(N_points, psi_analytical.data(), Config::x_start, Config::x_end, grid.data());
+    // solver.analytical_method(N_points, psi_analytical.data(), Config::x_start, Config::x_end, grid.data());
 
     // call function to compute Runge-Kutta 4th order approach
     solver.runge_kutta4(N_points, psi_RK.data(), Config::y_0, Config::alpha, Config::x_start, Config::x_end, Config::P, Config::Q, grid.data());
 
     // call function to compute error for each numerical metho
-    solver.error_compute(N_points, E_euler.data(), E_rk.data(), psi.data(), psi_analytical.data(), psi_RK.data());
+    // solver.error_compute(N_points, E_euler.data(), E_rk.data(), psi.data(), psi_analytical.data(), psi_RK.data());
 
 
-    // // for-loop to get results of each appraoch
-    // for (int i=0; i<N_points; i++){
-    //     std::cout << "Euler's Method: ";
-    //     std::cout << "x:" << grid[i] << " , " << "y:" << psi[i] << "\n";
+    // for-loop to get results of each appraoch
+    for (int i=0; i<N_points; i++){
+        std::cout << "Euler's Method: ";
+        std::cout << "x:" << grid[i] << " , " << "y:" << psi[i] << "\n";
 
-    //     std::cout << std::endl;
+        std::cout << std::endl;
         
-    //     std::cout << "Anlytical Method: ";
-    //     std::cout << "x:" << grid[i] << " , " << "y:" << psi_analytical[i] << "\n";
+        // std::cout << "Anlytical Method: ";
+        // std::cout << "x:" << grid[i] << " , " << "y:" << psi_analytical[i] << "\n";
 
-    //     std::cout << std::endl;
+        // std::cout << std::endl;
 
-    //     std::cout << "Runge-Kutta Method:";
-    //     std::cout << "x:" << grid[i] << " , " << "y:" << psi_RK[i] << "\n";
+        std::cout << "Runge-Kutta Method:";
+        std::cout << "x:" << grid[i] << " , " << "y:" << psi_RK[i] << "\n";
 
-    //     std::cout << std::endl;
+        std::cout << std::endl;
 
-    //     std::cout << "========================\n";
-    // }
-
-    std::ofstream out_file("bernoulli_results.csv");
-    out_file << "x,Euler,Analytical,RK4,Error_Euler,Error_RK4\n";
-
-    for (int i = 0; i < N_points; i++) {
-        if (i > 0 && psi[i] == 0.0 && psi_RK[i] == 0.0) {
-            break;
-        }
-
-        out_file << grid[i] << ","
-                 << psi[i] << ","
-                 << psi_analytical[i] << ","
-                 << psi_RK[i] << ","
-                 << E_euler[i] << ","
-                 << E_rk[i] << "\n";
+        std::cout << "========================\n";
     }
-    out_file.close();
+    
 
-    std::cout << "\n[Success] Data and errors successfully exported to 'bernoulli_results.csv'\n";
+// =======================================================================================================
+
+    // std::ofstream out_file("bernoulli_results.csv");
+    // out_file << "x,Euler,Analytical,RK4,Error_Euler,Error_RK4\n";
+
+    // for (int i = 0; i < N_points; i++) {
+    //     if (i > 0 && psi[i] == 0.0 && psi_RK[i] == 0.0) {
+    //         break;
+    //     }
+
+    //     out_file << grid[i] << ","
+    //              << psi[i] << ","
+    //              << psi_analytical[i] << ","
+    //              << psi_RK[i] << ","
+    //              << E_euler[i] << ","
+    //              << E_rk[i] << "\n";
+    // }
+    // out_file.close();
+
+    // std::cout << "\n[Success] Data and errors successfully exported to 'bernoulli_results.csv'\n";
 }
